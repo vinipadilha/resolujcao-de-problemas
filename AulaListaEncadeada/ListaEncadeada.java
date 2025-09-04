1f840744841050e57b3f6bafca215af12f444e0c
Vinicius <viniciusdarosapadilha@gmail.com>
2025-09-04 10:15:53 -0300

    Início ex de Java

---
+++ AulaListaEncadeada/ListaEncadeada.java
@@ -0,0 +1,38 @@
+import org.w3c.dom.Node;
+
+public class ListaEncadeada {
+    private Node Lista;
+
+    public ListaEncadeada() {
+        Lista = null;
+
+    }
+
+    public void vazia() {
+
+    }
+    public void inserePrimeiro(int info){
+
+    }
+    public void insereDepois(Node node, int info){
+
+    }
+    public void insereUltimo(int info){
+
+    }
+    public void insereOrdenado(Node node, int info){
+
+    }
+    public void imprime(){
+
+    }
+    public void removePrimeiro(){
+
+    }
+    public void removeUltimo(){
+
+    }
+    public void remove( Node no ){
+
+    }
+}
