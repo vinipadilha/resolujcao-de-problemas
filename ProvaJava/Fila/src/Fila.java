a894343e1296b6ed53b5dfa49bbf222ca29fb82e
Vinicius <viniciusdarosapadilha@gmail.com>
2025-09-24 00:35:23 -0300

    Adiciona métodos de vazia e cheia

--- ProvaJava/Fila/src/Fila.java
+++ ProvaJava/Fila/src/Fila.java
@@ -1,2 +1,24 @@
 public class Fila {
+    private int primeiro;
+    private int ultimo;
+    private int dados[];
+    private int capacidade;
+    private int quantidade;
+
+    public Fila(int capacidade){
+        this.capacidade = capacidade;
+        this.primeiro = 0;
+        this.ultimo = -1;
+        this.quantidade = 0;
+        this.dados = new int[capacidade]
+    }
+
+    public boolean vazia(){
+        return quantidade == 0;
+    }
+
+    public boolean cheia(){
+        return quantidade == capacidade;
+    }
+
 }
